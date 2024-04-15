80a2fb9c57a25ab847a9a7ac5c24cb696f5d63cf
jrsmth <jamesrichardsmith97@gmail.com>
2024-04-15 16:39:06 +0100

    [LIT-22] Add datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './presentation/calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
